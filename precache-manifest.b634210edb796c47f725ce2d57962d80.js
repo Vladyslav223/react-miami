6050a77799b6ead1c7ef6115fdb7b9ebddacfcef
vladyslav223 <vladyslav223@gmail.com>
2019-09-05 20:10:08 +0300

    Updates

---
+++ precache-manifest.b634210edb796c47f725ce2d57962d80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d6775c365af54afff4d30ee653ffa7",
+    "url": "/react-miami/index.html"
+  },
+  {
+    "revision": "39cc933853216eed2866",
+    "url": "/react-miami/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c215f01e31e42b7f4fce",
+    "url": "/react-miami/static/js/2.d82308d0.chunk.js"
+  },
+  {
+    "revision": "39cc933853216eed2866",
+    "url": "/react-miami/static/js/main.50629b8e.chunk.js"
+  },
+  {
+    "revision": "3bf9cdaac47e3d270118",
+    "url": "/react-miami/static/js/runtime~main.d51db64a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-miami/static/media/logo.5d5d9eef.svg"
+  }
+]);
